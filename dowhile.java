40f6d44f7e90f7001c578714f940c5b0d4ffdc24
Viraj Pawar <115443772+vraj2730@users.noreply.github.com>
2022-10-11 15:20:01 +0530

    New File Added

---
+++ dowhile.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class dowhille
+{
+    public static void main(String[]args)
+    {
+        Scanner vp=new Scanner(System.in);
+
+        System.out.println("Enter the no. you want");
+        int n=vp.nextInt();
+        for (int i=1;i<=n;i++)
+        {
+            System.out.println(i);
+        }
+
+    }
+}
