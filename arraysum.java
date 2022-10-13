92c1c5421864045508be7c4a0b351440888d5af2
Viraj Pawar <115443772+VrajPawar@users.noreply.github.com>
2022-10-13 22:11:33 +0530

    New program added

---
+++ arraysum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class arraysum
+{
+    public static void main(String[] args)
+    {
+        float []abc={2.3f,3.4f,5.6f,34.0f,66.6f};
+                float sum=0;
+                for (float total:abc)
+                {
+                    sum=sum+total;
+
+                }
+        System.out.println("total is"+sum);
+    }
+}
+
+
+
