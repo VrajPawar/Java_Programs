a2865c367f7cd8bfea44920b861131aa0227d731
Viraj Pawar <115443772+VrajPawar@users.noreply.github.com>
2022-10-15 19:23:59 +0530

    Array Program

---
+++ array4.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+public class array4
+{
+
+    public static void main(String[] args)
+    {
+        int []abc={2,3,4,5,6,7};
+        float num=2;
+
+           if (abc==num)
+           {
+
+           }
+
+        System.out.println("total is");
+    }
+}
